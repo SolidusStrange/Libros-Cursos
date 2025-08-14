cde566347c92d1f1144e06c9e6338896d3548673
José Díaz <155374823+SolidusStrange@users.noreply.github.com>
2025-08-13 23:24:23 -0400

    Add files via upload

--- Cisco Networking Academy/test.py
+++ Cisco Networking Academy/test.py
@@ -1,33 +1,19 @@
-lista_inicial = [
-    [1, 2, 3],
-    [4, "X", 6],
-    [7, 8, 9]
-]
+# board= [[i for i in range(1,10)] for j in range(1,10)]
+# print(board)
 
-def imprimir_tablero(matriz):
-    print("+-------+-------+-------+")
-    for fila in matriz:
-        print("|       |       |       |")
-        print("|" + "|".join(f" {str(valor).center(5)} " for valor in fila) + "|")
-        print("|       |       |       |")
-        print("+-------+-------+-------+")
+board = []
+counter = 1
 
-# imprimir_tablero(lista_inicial)
+board = [["X" for i in range(3)] for j in range(3)]
 
-def movimiento(usuario, matriz):
-    for fila in range(len(matriz)):
-        for columna in range(len(matriz[fila])):
-            if matriz[fila][columna] == usuario:
-                matriz[fila][columna] = "O" # Si es así, reemplazar por "X" y salir de la función
-                return True # movimiento válido
+counter = 0
+for i in range(3):
+    for j in range(3):
+        counter += 1
+        board[i][j] = counter
+print(board)
 
-    print("El movimiento ingresado no es válido") #Si no fue válido, salta el return y pasa a imprimir este msj
-    return False #Retorna false para el IF que evalua
+for r in board:
+    print(r)
 
-
-usuario = int(input("Ingrese un movimiento válido"))
-if movimiento(usuario, lista_inicial):
-    imprimir_tablero(lista_inicial)
-
-
-
+print("+-------+\n|\n|\n|\n+-------+")  
