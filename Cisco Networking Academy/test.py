b0c4aafead379ea1dbcc22afa830529af62195d7
José Díaz <155374823+SolidusStrange@users.noreply.github.com>
2025-08-17 22:53:12 -0400

    Update test.py

--- Cisco Networking Academy/test.py
+++ Cisco Networking Academy/test.py
@@ -1,19 +1,70 @@
-# board= [[i for i in range(1,10)] for j in range(1,10)]
-# print(board)
+import random
 
-board = []
-counter = 1
+tablero = [
+    [1, 2, 3],
+    [4, "X", 6],
+    [7, 8, 9]
+]
 
-board = [["X" for i in range(3)] for j in range(3)]
+def imprimir_tablero(matriz):
+    print("+-------+-------+-------+")
+    for fila in matriz:
+        print("|       |       |       |")
+        print("|" + "|".join(f" {str(valor).center(5)} " for valor in fila) + "|")
+        print("|       |       |       |")
+        print("+-------+-------+-------+")
 
-counter = 0
-for i in range(3):
-    for j in range(3):
-        counter += 1
-        board[i][j] = counter
-print(board)
+# imprimir_tablero(tablero)
 
-for r in board:
-    print(r)
+def movimiento(usuario, matriz):
+    for fila in range(len(matriz)):
+        for columna in range(len(matriz[fila])):
+            if matriz[fila][columna] == usuario:
+                matriz[fila][columna] = "O" # Si es así, reemplazar por "X" y salir de la función
+                return True # movimiento válido
 
-print("+-------+\n|\n|\n|\n+-------+")  
+    print("El movimiento ingresado no es válido. Intente nuevamente") #Si no fue válido, salta el return y pasa a imprimir este msj
+    return False #Retorna false para el IF que evalua
+
+def maquina_movimiento(matriz):
+    # maquina = random.choice(tablero)
+    disponibles = [] #Buscar todos los valores que sean números (casillas libres)
+    for fila in range(len(matriz)):
+        for columna in range(len(matriz[fila])):
+            if isinstance(matriz[fila][columna], int): # solo números libres
+                disponibles.append((fila, columna))
+            
+    #Elegir una posición al azar
+    if disponibles: #disponibles guarda tuplas (fila, columna) de todas las casillas libres.
+        fila, columna = random.choice(disponibles) #elige una de esas posiciones
+        matriz[fila][columna] = "X"
+        return True
+    return False
+
+def revisar_ganador(matriz):
+    for fila in range(3):
+        if all(valor == "X" for valor in matriz[fila]):
+            print(F"Fila {fila+1} completa con X")
+
+    for columna in range(3):
+        if all(matriz[fila][columna] == "X" for fila in range(3)):
+            print(f"Columna {columna+1} completa en X")
+        
+    if all(matriz[i][i] == "X" for i in range(3)): #[0][0], [1][1], [2][2]
+        print("Diagonal principal completa con X")
+
+    #posiciones [0][2], [1][1], [2][0]
+    if all(matriz[i][2-i] == "X" for i in range(3)):
+        print("Diagonal secundaria completa con X")
+        
+        
+revisar_ganador(tablero)
+
+# while True:
+#     usuario = int(input("Ingrese un movimiento válido: "))
+#     if movimiento(usuario, tablero):
+#         imprimir_tablero(tablero)
+#         break
+
+
+
